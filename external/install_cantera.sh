be157e599d3ef5dad1ea2a6b37bc81327ab7c5ea
stormy/cli <chengcli@umich.edu>
2024-02-12 13:51:14 -0500

    wip

--- external/install_cantera.sh
+++ external/install_cantera.sh
@@ -2,20 +2,20 @@
 
 CXX=g++
 CC=gcc
-cxx_flags=-std=c++14
+cxx_flags=-std=c++17
 prefix=${HOME}/opt/
 python_package=n
 f90_interface=n
 system_eigen=y
 extra_inc_dirs=${HOME}/opt/include/eigen3
 system_blas_lapack=n
-boost_inc_dir=`pwd`/cliboost/
+boost_inc_dir=`pwd`/ext/cliboost
 
-cd cantera
-scons build CXX=${CXX} CC=${CC} cxx_flags=${cxx_flags} prefix=${prefix} \
+#cd cantera
+scons build -j8 CXX=${CXX} CC=${CC} cxx_flags=${cxx_flags} prefix=${prefix} \
   python_package=${python_package} f90_interface=${f90_interface} \
   system_eigen=${system_eigen} extra_inc_dirs=${extra_inc_dirs} \
   system_blas_lapack=${system_blas_lapack} \
   boost_inc_dir=${boost_inc_dir}
 scons install
-cd ..
+#cd ..
